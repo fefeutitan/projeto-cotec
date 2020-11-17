0bb9ed343ff29e32782753880f87148348afc179
Fernando Ellery <fernando.ellery@seinfra.ce.gov.br>
2020-11-17 11:38:26 -0300

    modal alert success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modulos/beneficiario/beneficiario-form/beneficiario-form.component.ts
+++ src/app/modulos/beneficiario/beneficiario-form/beneficiario-form.component.ts
@@ -1,55 +1,77 @@
+import { AlertModalService } from './../../../shared/alert-modal.service';
 import { BeneficiarioService } from './../../beneficiario.service';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-beneficiario-form',
   templateUrl: './beneficiario-form.component.html',
-  styleUrls: ['./beneficiario-form.component.scss']
+  styleUrls: ['./beneficiario-form.component.scss'],
 })
 export class BeneficiarioFormComponent implements OnInit {
-
   form: FormGroup;
   submitted = false;
 
-  constructor(private fb: FormBuilder, private service: BeneficiarioService) { }
+  constructor(
+    private fb: FormBuilder,
+    private service: BeneficiarioService,
+    private modal: AlertModalService,
+    private location: Location
+  ) {}
 
-  ngOnInit(): void {
-
+  ngOnInit() {
     this.form = this.fb.group({
-      nome: [null,[Validators.required, Validators.minLength(3), Validators.maxLength(30)]],
-      cpf: [null,[Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-      orgao: [null,[Validators.required]],
-      matricula: [null,[Validators.required, Validators.minLength(3), Validators.maxLength(30)]]
+      nome: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
+      cpf: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(11),
+          Validators.maxLength(14),
+        ],
+      ],
+      orgao: [null, [Validators.required]],
+      matricula: [
+        null,
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(30),
+        ],
+      ],
     });
   }
 
   hasError(field: string) {
-
     return this.form.get(field).errors;
-
   }
 
   onSubmit() {
-
     this.submitted = true;
     console.log(this.form.value);
     if (this.form.valid) {
       console.log('submit');
       this.service.create(this.form.value).subscribe(
-        success => console.log('sucesso'),
-        error => console.error(error),
+        (success) => {
+          this.modal.showAlertSuccess('Crido com sucesso!');
+          this.location.back();
+        },
+        (error) => this.modal.showAlertDanger('Erro Ao criar beneficiário!'),
         () => console.log('request completo')
       );
-     }
-
+    }
   }
 
   onCancel() {
-
     this.submitted = false;
     this.form.reset();
-
   }
-
 }
