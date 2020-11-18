270c6c41514ef61c95242f6f0d7ba3208eadfb8b
Fernando Ellery <fernando.ellery@seinfra.ce.gov.br>
2020-11-18 15:56:35 -0300

    upload

--- src/app/modulos/processo/processo.component.ts
+++ src/app/modulos/processo/processo.component.ts
@@ -19,58 +19,58 @@ export class ProcessoComponent implements OnInit {
 
   ngOnInit() { }
 
-  onChange(event) {
-    console.log(event);
+  // onChange(event) {
+  //   console.log(event);
 
-    const selectedFiles = <FileList>event.srcElement.files;
-    // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
+  //   const selectedFiles = <FileList>event.srcElement.files;
+  //   // document.getElementById('customFileLabel').innerHTML = selectedFiles[0].name;
 
-    const fileNames = [];
-    this.files = new Set();
-    for (let i = 0; i < selectedFiles.length; i++) {
-      fileNames.push(selectedFiles[i].name);
-      this.files.add(selectedFiles[i]);
-    }
-    document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
+  //   const fileNames = [];
+  //   this.files = new Set();
+  //   for (let i = 0; i < selectedFiles.length; i++) {
+  //     fileNames.push(selectedFiles[i].name);
+  //     this.files.add(selectedFiles[i]);
+  //   }
+  //   document.getElementById('customFileLabel').innerHTML = fileNames.join(', ');
 
-    this.progress = 0;
-  }
+  //   this.progress = 0;
+  // }
 
-  onUpload() {
-    if (this.files && this.files.size > 0) {
-      this.service.upload(this.files, environment.BASE_URL + '/upload')
-        .pipe(
-          uploadProgress(progress => {
-            console.log(progress);
-            this.progress = progress;
-          }),
-          filterResponse()
-        )
-        .subscribe(response => console.log('Upload Concluído'));
-        // .subscribe((event: HttpEvent<Object>) => {
-        //   // console.log(event);
-        //   if (event.type === HttpEventType.Response) {
-        //     console.log('Upload Concluído');
-        //   } else if (event.type === HttpEventType.UploadProgress) {
-        //     const percentDone = Math.round((event.loaded * 100) / event.total);
-        //     // console.log('Progresso', percentDone);
-        //     this.progress = percentDone;
-        //   }
-        // } );
-    }
-  }
+  // onUpload() {
+  //   if (this.files && this.files.size > 0) {
+  //     this.service.upload(this.files, environment.BASE_URL + '/upload')
+  //       .pipe(
+  //         uploadProgress(progress => {
+  //           console.log(progress);
+  //           this.progress = progress;
+  //         }),
+  //         filterResponse()
+  //       )
+  //       .subscribe(response => console.log('Upload Concluído'));
+  //       // .subscribe((event: HttpEvent<Object>) => {
+  //       //   // console.log(event);
+  //       //   if (event.type === HttpEventType.Response) {
+  //       //     console.log('Upload Concluído');
+  //       //   } else if (event.type === HttpEventType.UploadProgress) {
+  //       //     const percentDone = Math.round((event.loaded * 100) / event.total);
+  //       //     // console.log('Progresso', percentDone);
+  //       //     this.progress = percentDone;
+  //       //   }
+  //       // } );
+  //   }
+  // }
 
-  onDownloadExcel() {
-    this.service.download(environment.BASE_URL + '/downloadExcel')
-    .subscribe((res: any) => {
-      this.service.handleFile(res, 'report.xlsx');
-    });
-  }
+  // onDownloadExcel() {
+  //   this.service.download(environment.BASE_URL + '/downloadExcel')
+  //   .subscribe((res: any) => {
+  //     this.service.handleFile(res, 'report.xlsx');
+  //   });
+  // }
 
-  onDownloadPDF() {
-    this.service.download(environment.BASE_URL + '/downloadPDF')
-    .subscribe((res: any) => {
-      this.service.handleFile(res, 'report.pdf');
-    });
-  }
+  // onDownloadPDF() {
+  //   this.service.download(environment.BASE_URL + '/downloadPDF')
+  //   .subscribe((res: any) => {
+  //     this.service.handleFile(res, 'report.pdf');
+  //   });
+  // }
 }
